6bba34d16bc81c8fe5e01086bfb0bea9cbfab3d8
geemoowy <kovylek.ul@gmail.com>
2025-05-24 11:18:59 +0300

    add app users

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mailings'
+    'mailings',
+    'users',
 ]
 
 MIDDLEWARE = [
