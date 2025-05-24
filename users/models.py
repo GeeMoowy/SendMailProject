4582a39010ce09b33b5c80ba8590c8eaf400aa3a
geemoowy <kovylek.ul@gmail.com>
2025-05-24 11:36:25 +0300

    add model CustomUser

--- users/models.py
+++ users/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True, verbose_name='Email')
+    avatar = models.ImageField(upload_to='avatars/', verbose_name='Фото', null=True, blank=True)
+    phone_number = models.CharField(max_length=15, verbose_name='Телефон', null=True, blank=True)
+    country = models.CharField(max_length=100, verbose_name='Страна', null=True, blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.email
